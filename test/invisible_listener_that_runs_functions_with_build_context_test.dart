792c736ec487f895db12da6d0b9a4172cae7eb2c
Mihail Paley <undeadlol1@mail.ru>
2022-03-24 13:04:29 +0500

    refactor: rename some files

---
+++ test/invisible_listener_that_runs_functions_with_build_context_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:run_function_with_context_anywhere/invisible_listener_that_runs_functions_with_build_context.dart';
+
+void main() {
+  group('', () {
+    test('adds one to input values', () {
+      // final calculator = Calculator();
+      // expect(calculator.addOne(2), 3);
+      // expect(calculator.addOne(-7), -6);
+      // expect(calculator.addOne(0), 1);
+      expect(true, isTrue);
+    });
+  });
+}
