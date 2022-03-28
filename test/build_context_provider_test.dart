ea70004b078ee6d34aa29f49222c1e4e4f4470b0
Mikhail Palei <mikhail_palei@epam.com>
2022-03-28 23:45:27 +0500

    feat: :white_check_mark: adds a test

--- test/build_context_provider_test.dart
+++ test/build_context_provider_test.dart
@@ -7,6 +7,7 @@ import 'package:build_context_provider/build_context_provider.dart';
 class _DummyClassToMock {
   void firstFunctionToTest(BuildContext context) {}
   void secondFunctionToTest(BuildContext context) {}
+  bool functionThatReturnsValue(BuildContext context) => true;
 }
 
 class _MockedDummyClass extends Mock implements _DummyClassToMock {}
@@ -17,7 +18,7 @@ final _mockedClassWithFunctions = _MockedDummyClass();
 
 void main() {
   group(
-    'Run function with context anywhere tests - ',
+    'Run function with build context tests - ',
     () {
       setUp(() {
         registerFallbackValue(_MockedBuildContext());
@@ -56,6 +57,31 @@ void main() {
           await tester.pump();
 
           verify(mockedFunctionCall).called(2);
+        },
+      );
+
+      testWidgets(
+        'WHEN a function is added '
+        'THEN returned result from function should be usable',
+        (tester) async {
+          bool aValueToBeChangedToTrue = false;
+          int howManyTimesHaveTheFunctionRan = 0;
+
+          bool functionThatReturnsValue(BuildContext context) {
+            ++howManyTimesHaveTheFunctionRan;
+            return true;
+          }
+
+          void aFunctionThatCapturesReturnedResult(BuildContext context) {
+            aValueToBeChangedToTrue = functionThatReturnsValue(context);
+          }
+
+          await _pumpWidget(tester);
+          BuildContextProvider.provideBuildContext(aFunctionThatCapturesReturnedResult);
+          await tester.pump();
+
+          expect(aValueToBeChangedToTrue, isTrue);
+          expect(howManyTimesHaveTheFunctionRan, 1);
         },
       );
     },
